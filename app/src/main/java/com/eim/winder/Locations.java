874168611fcb7fe2f8dd98d75d3a9d9d2fb370f0
Mari Thorsteinsen <mari_andrine@live.no>
2016-02-04 00:33:09 +0100

    layout update

--- app/src/main/java/com/eim/winder/Locations.java
+++ app/src/main/java/com/eim/winder/Locations.java
@@ -4,5 +4,5 @@ package com.eim.winder;
  * Created by Mari on 31.01.2016.
  */
 public class Locations {
-    public static final String[] locations = {"Trondheim Sentrum", "Lade", "Ila", "Orkanger", "Stjørdal"};
+    public static final String[] locations = {"Trondheim Sentrum", "Lade", "Ila", "Orkanger", "Stjørdal","Hatmountainvalley", "Åre", "UtiGokk"};
 }
