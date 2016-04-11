01ff1d1f67fa7c1bfaa3f4af5ce3f4f4b95e726c
Mari Thorsteinsen <mari_andrine@live.no>
2016-04-11 14:47:00 +0200

    ny xml url NORSK

--- app/src/main/java/com/eim/winder/activities/EventListFragment.java
+++ app/src/main/java/com/eim/winder/activities/EventListFragment.java
@@ -7,14 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.eim.winder.R;
+import com.eim.winder.db.AlertSettingsDAO;
+import com.eim.winder.db.ForecastDAO;
+import com.eim.winder.db.ForecastDSService;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EventListFragment extends Fragment {
+    private ListView eventList;
+    private ArrayList<ForecastDAO> forecastList;
+    private ArrayAdapter listAdapter;
+    private ForecastDSService datasource;
+    private AlertSettingsDAO alertSettingsDAO;
 
 
     public EventListFragment() {
@@ -25,8 +38,13 @@ public class EventListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        datasource = new ForecastDSService(getContext());
+        View v = inflater.inflate(R.layout.fragment_event_list, container, false);
+        alertSettingsDAO = ((AlertOverViewActivity) getActivity()).getAlertSettingsDAO();
+        forecastList = datasource.getAllForecastsByAlertSettingsID(alertSettingsDAO.getId());
+        eventList = (ListView) getActivity().findViewById(R.id.event_listview);
 
-        return inflater.inflate(R.layout.fragment_event_list, container, false);
+        return v;
     }
 
 }
